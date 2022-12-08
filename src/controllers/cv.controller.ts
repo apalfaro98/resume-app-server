1ecf9878ffea707f71d010a089932b986fa79c59
Angelica Perez <apalfaro9803@gmail.com>
2022-12-08 01:41:56 -0400

    save without image

--- src/controllers/cv.controller.ts
+++ src/controllers/cv.controller.ts
@@ -3,9 +3,22 @@ import Resume from '../models/cv.model';
 
 const cvController = {
     create: async (req: Request, res: Response) => {
-        const body = req.body;
+        const { name, abilities, dateOfBirth, age, email, ...body} = req.body;
+        const phone = (body.phone) ? body.phone : undefined;
+        const experiences = (body.experiences) ? body.experiences : undefined;
+        const resume = new Resume({
+            name,
+            abilities,
+            dateOfBirth,
+            age,
+            email,
+            phone,
+            experiences
+        })
+        await resume.save();
         return res.status(201).json({
             created: true,
+            result: resume
         });
     },
 };

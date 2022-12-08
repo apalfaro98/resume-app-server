730f4ac0dd5ee2420679e15326565bd5310927bb
Angelica Perez <apalfaro9803@gmail.com>
2022-12-08 12:09:04 -0400

    controller for user creation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,24 @@
 import { Request, Response } from "express";
 import User from '../models/user.model';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userController = {
     create: async (req: Request, res: Response) => {
-        // const resume = new Resume({
-        //     name,
-        //     abilities,
-        //     dateOfBirth,
-        //     age,
-        //     email,
-        //     phone,
-        //     experiences,
-        //     imageUrl
-        // })
-        // await resume.save();
+        const {email, password} = req.body;
+        const user = new User({
+            email,
+            password
+        })
+
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync(password, salt);
+
+        await user.save();
+
         return res.status(201).json({
             created: true,
+            result: user
         });
     },
 };
